4d051f6c74171c2ebd390188dd0909703b763e4b
sunamo.cz <radek.jancik@sunamo.cz>
2024-07-16 09:25:24 +0200

    Before removing all *SunamoExceptions.cs

--- _sunamo/SHSplit.cs
+++ _sunamo/SHSplit.cs
@@ -8,7 +8,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 namespace SunamoBazosCrawler._sunamo;
+
 internal class SHSplit
 {
     internal static List<string> SplitMore(string p, params string[] newLine)
