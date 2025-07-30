923fd039d544a60379b0ad07fac22d7f6f8abccd
sunamo.cz <radek.jancik@sunamo.cz>
2025-07-30 23:29:10 +0200

    Auto-commit before force push

--- SunamoBazosCrawler/_sunamo/HtmlAssistant.cs
+++ SunamoBazosCrawler/_sunamo/HtmlAssistant.cs
@@ -2,6 +2,22 @@ namespace SunamoBazosCrawler._sunamo;
 
 internal class HtmlAssistant
 {
+    internal static string GetValueOfAttribute(string p, HtmlNode divMain, bool _trim = false)
+    {
+        object o = divMain.Attributes[p]; // divMain.GetAttributeValue(p, null);//
+        if (o != null)
+        {
+            var st = ((HtmlAttribute)o).Value;
+            if (_trim) st = st.Trim();
+
+            if (st == string.Empty) return "(null)";
+
+            return st;
+        }
+
+        return string.Empty;
+    }
+
     internal static string InnerText(HtmlNode node, bool recursive, string tag, string attr, string attrValue,
         bool contains = false)
     {
